9112b70aaf049d24f6aa6e3f287275dd56e5f7a5
troymeeker <troymeeker1@gmail.com>
2021-07-07 14:10:48 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+
+let random = Math.ceil(Math.random()*10);
+
+const num3 = 18;
+const num4 = 7;
+
+let mod = num3%num4;
+
+
+let max = Math.max(1,5,20,12,2);
+ max;
+//returns max # from a set, num is 20
